e5ea4d4f35bb91ba5afd913e4b14be8636b5652a
Max29xD <marcos291198@gmail.com>
2024-11-02 19:52:06 -0400

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+import fluid, { extract, screens, fontSize } from "fluid-tailwind";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: {
+    files: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    extract,
+  },
+  theme: {
+    screens,
+    fontSize,
+    extend: {},
+  },
+  plugins: [fluid],
+};
